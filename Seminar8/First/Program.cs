7f9d195726b2099154d46c9d0f8266880a24308c
Anton <anton-semenov@mail.ru>
2022-09-30 21:05:23 +0300

    rename seminar8

---
+++ Seminar8/First/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (n == a*a)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
