f7f2343f1336f8ff990f341159c9c212cc35676a
Anton <anton-semenov@mail.ru>
2022-10-01 17:48:06 +0300

    сохранил все изменения

---
+++ Seminar8/Second/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Console.Write("Введите номер дня недели (от 1 до 7): ");
+int weekDay=Convert.TOInt32(Console.ReadLine());
+while (weekDay<1|weekDay>7)
+{
+    Console.Write("Вы ввели недопустимый номер дня недели, повторите ввод (от 1 до 7): ");
+    a=Convert.ToInt32(Console.ReadLine());
+    i++;
+    if (i>9) Console.WriteLine("читайте условие внимательно!");
+}
+switch (a)
+{
+    case 1:Console.WriteLine("Понедельник");
+    break;
+    case 2:Console.WriteLine("Вторник");
+    break;
+    case 3:Console.WriteLine("Среда");
+    break;
+    case 4:Console.WriteLine("Четверг");
+    break;
+    case 5:Console.WriteLine("Пятница");
+    break;
+    case 6:Console.WriteLine("Суббота");
+    break;
+    case 7:Console.WriteLine("Воскресенье");
+    break;
+    Дальше дописать!!!!!!
+}
+*/
+
+Console.WriteLine("Введите трехзначное число N: ");
+int n=Convert.ToInt32(Console.ReadLine());
+While (n<100|n>999); // | логическое или
+                    // & логическое и
+                    // ! логическое не
+Console.Write("Введите т");
+int i = -n;
+if (i<=n);
